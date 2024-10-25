b880e1df980ace311ae0fe50dd95b9e6dd4584aa
Elver <holahacker01@gmail.com>
2024-10-24 23:15:04 -0500

    Initial Commit

---
+++ RetoApuestaTotal/RetoApuestaTotalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetoApuestaTotalApp.swift
+//  RetoApuestaTotal
+//
+//  Created by Elver Mayta Hernández on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct RetoApuestaTotalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
